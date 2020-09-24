5effdbf0805115742cf2bbfa8c3430b457c9e9d1
MVP-crypto <65507645+MVP-crypto@users.noreply.github.com>
2020-09-24 11:53:07 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,16 @@
-print("hello")
-print("number 1 for addition")
-print("number 2 for substraction")
-print("number 3 for factorisation")
-print("number 4 for devision")
+print("number 1 addition")
+print("number 2 substraction")
+print("number 3 factor")
 x=1
 while x==1:
-    choice=int(input("enter your choice here: ")
-    num1 = int(input("enter first number: ")
-    num2 = int(input("enter second number: ")
-    if choice == 1:
-              print(num1+num2)
-    if choice == 2:
-              print(num1-num2)
-    if choice == 3:
-              print(num1*num2)
+    choice=int(input("enter here: "))
+    num1=int(input("enter first number: "))
+    num2=int(input("enter second number: "))
+    if choice==1:
+        print(num1+num2)
+    if choice==2:
+        print(num1-num2)
+    if choice==3:
+        print(num1*num2)
     if choice == 4:
-              print(num1/num2)
-          
-
-
+        print(num1/num2)
